ed24c334252f7e43bf3d26d5e2d5899e3bc07c9b
Diane Choi <ekska21@gmail.com>
2025-05-13 19:46:25 -0700

    Add youtube music downloader and update csv file with audio file path

---
+++ youtube_audio_download.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from youtube_search import YoutubeSearch
+import yt_dlp
+import re
+
+def sanitize_file_name(file_name):
+  file_name = re.sub(r'[\/:*?"<>|]', '_', file_name)
+  file_name = re.sub(r'\s+', '_', file_name)
+  file_name = re.sub(r'_+', '_', file_name)
+  file_name = file_name.strip()
+  return file_name
+  
+def is_relevant_video_title(video_title, song_title, song_artist):
+  video_title = video_title.lower()
+  song_title = song_title.lower()
+  song_artist = song_artist.lower()
+  
+  title_words = song_title.split()
+  artist_words = song_artist.split()
+  
+  title_matches = sum([word in video_title for word in title_words])
+  artist_matches = sum([word in video_title for word in artist_words])
+  
+  return title_matches >= len(title_words) * 0.5 and artist_matches >= len(artist_words) * 0.5
+  
+def download_song(title, artist):
+  query = f"{title} {artist} audio"
+  file_name = sanitize_file_name(f"{title}-{artist}")
+  
+  # search Youtube
+  videos_search = YoutubeSearch(query, max_results=5)
+  results = videos_search.to_dict()
+    
+  for searched in results:
+    video = searched
+    video_title = video['title']
+    
+    if is_relevant_video_title(video_title, title, artist):
+      video_url = f"https://www.youtube.com{searched['url_suffix']}"
+        
+      # set up yt_dlp
+      ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+        }],
+        'ffmpeg_location': '/usr/local/bin',
+        'outtmpl': f'./audios/{file_name}.%(ext)s'
+      }
+        
+      # download the audio file
+      with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+      print(f"Successfully download the audio file for: {title} by {artist}")
+      return f'./audios/{file_name}.mp3'
+  else:
+    print(f"Cannot find the audio file for: {title} by {artist}")
+  return None
+      
+def download_music_from_youtube(csv_file):
+  # Read csv file
+  df = pd.read_csv(csv_file, delimiter=';', encoding='UTF-8')
+  
+  audios = []
+  
+  # search Youtube and download the audio file
+  for index, row in df.iterrows():
+    title = row['Title']
+    artist = row['Artist']
+    
+    audio_file_path = download_song(title, artist)
+    audios.append(audio_file_path)
+  
+  df['Audio'] = audios
+  df = df.fillna('None')
+  print(df)
+  
+  df.to_csv(csv_file, index=False, encoding='UTF-8', sep=';')
+    
+
+if __name__ == '__main__':
+  csv_file = '/Users/dianna/Desktop/CST/After/Playlist_Generator/playlist_csv/80s_hiphop.csv'
+  download_music_from_youtube(csv_file)
