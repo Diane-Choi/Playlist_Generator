10501c8f8f4aaa142c2084aca245405f95b9bfe0
Diane Choi <ekska21@gmail.com>
2025-05-14 01:38:58 -0700

    Implement a function to write a prompt for image generation

--- dalle3_image_generation.py
+++ dalle3_image_generation.py
@@ -6,6 +6,27 @@ load_dotenv()
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def text_to_image_prompt_generator(song_title, artist):
+  response = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+    temperature=0.5,
+    messages=[
+      {
+        'role': 'system', 
+        'content': """
+          You are an AI assistant designed to create DALL·E-2 prompts.
+
+          When the user provides information about a song, imagine the song's lyrics and the representative image of its mood.
+          Based on the imagined image, generate a text prompt for the text-to-image model, DALL·E-2.
+          """
+      },
+      {'role': 'user', 'content': 'Stronger - Kelly Clarkson'}, # Give an example 
+      {'role': 'assistant', 'content': 'Create an image of a person standing on top of a mountain, surrounded by vibrant rays of sunlight, radiating confidence and strength.'}, # Give an example response
+      {'role': 'user', 'content': f'{song_title} by {artist}'},
+    ],
+  )
+  return response.choices[0].message.content
+
 def generate_dalle_image(prompt, image_file_name, size="512x512"):
   img = client.images.generate(
       model="dall-e-2",
@@ -28,5 +49,11 @@ def generate_dalle_image(prompt, image_file_name, size="512x512"):
       
   return image_file_path
 
-result = generate_dalle_image("A baby eating subway cookie", "sss")
+
+song_title = "Gangnam Style"
+artist = "PSY"
+prompt = text_to_image_prompt_generator(song_title, artist)
+print(prompt)
+
+result = generate_dalle_image('Create a Photo. ' + prompt, f'{song_title}-{artist}')
 print(result)
