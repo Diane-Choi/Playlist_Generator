55fe5da8a0eba13e843351012a89d00b16acdb1f
Diane Choi <ekska21@gmail.com>
2025-05-17 20:05:24 -0700

    Add a function to generate videos using audio and image paths from csv

--- generate_video.py
+++ generate_video.py
@@ -31,10 +31,33 @@ def create_video(audio_path, image_path):
   video.write_videofile(output_path, codec='libx264', fps=1)
   
   return output_path
+
+def create_videos_from_csv_playlist(csv_file):
+  """
+  Creates a video for each of the song in the playlist csv file that has the following columns:
+    - 'Audio': The file path of the audio file (e.g. mp3)
+    - 'Info_Image': The file path of the image that represents the song info.
+  
+  Args:
+    csv_file (str): A file path of the csv file (separated by ;)
+    
+  Returns:
+    list[str]: A list of successfully created video paths
+  """
+  df_playlist = pd.read_csv(csv_file, sep=';')
+  videos = list()
+  
+  for i, row in df_playlist.iterrows():
+    if row['Audio'] != 'Not Found':
+      video = create_video(row['Audio'], row['Info_Image'])
+      videos.append(video)
+  
+  return videos
   
   
 if __name__ == '__main__':
   audio_path = '/Users/dianna/Desktop/CST/After/Playlist_Generator/audios/I_Am_the_Best-2NE1.mp3'
   image_path = '/Users/dianna/Desktop/CST/After/Playlist_Generator/images/I_Am_the_Best_2NE1__info.png'
-  result = create_video(audio_path, image_path)
-  print(result)
+  # result = create_video(audio_path, image_path)
+  videos = create_videos_from_csv_playlist('/Users/dianna/Desktop/CST/After/Playlist_Generator/playlist_csv/2010_kpop.csv')
+  print(videos)
